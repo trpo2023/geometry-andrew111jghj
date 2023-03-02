740fa808b7b671eea1a6d11327101252aff24d47
Andrew Firsov <firstmestag@gmail.com>
2023-03-02 21:27:10 +0700

    Add lower-case function

--- main.cpp
+++ main.cpp
@@ -2,16 +2,32 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
+
+void to_lower(std::string& s);
 
 int main() {
     string fp_iden = "Input.txt";
     ifstream fp;
     fp.open(fp_iden);
-    if (fp.is_open())
-        printf("Success");
-    else printf("Failed to open file");
+    if (fp.is_open()) {
+        string strr;
+        while (getline(fp, strr)) {
+
+            to_lower(strr);
+            cout << strr << endl;
+        }
+    }
 
     return 0;
+}
+
+void to_lower(std::string& s)
+{
+    std::transform(s.begin(), s.end(), s.begin(), [](unsigned char c) {
+        return std::tolower(c);
+        });
 }
