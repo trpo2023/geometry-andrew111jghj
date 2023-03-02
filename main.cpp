5fbf5d29f989e35332af9191828742817f1986b4
Andrew Firsov <firstmestag@gmail.com>
2023-03-02 21:30:18 +0700

    Add struct Coord to define point

--- main.cpp
+++ main.cpp
@@ -9,6 +9,40 @@ using namespace std;
 
 void to_lower(std::string& s);
 
+struct Coord {
+    int x;
+    int y;
+
+    static Coord convert_symbol(string& str)
+    {
+        Coord dot = { 0, 0 };
+        string val = "";
+        bool x_true = false;
+        for (size_t i = 0; i < str.length(); i++) {
+            if ((str[i] == ' ' && str[i + 1] != ' ') || i == str.length() - 1) {
+                if (i == str.length() - 1) {
+                    val += str[i];
+                }
+                if (!x_true) {
+                    dot.x = stoi(val);
+                    x_true = true;
+                }
+                else {
+                    dot.y = stoi(val);
+                }
+                val.clear();
+            }
+
+            if (str[i] == ' ') {
+                continue;
+            }
+            val += str[i];
+        }
+        return dot;
+    }
+};
+
+
 int main() {
     string fp_iden = "Input.txt";
     ifstream fp;
