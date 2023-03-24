d401bb052c01d7ff1f25ac7aab42386a7672c9e9
Andrey Firsov <firstmestag@gmail.com>
2023-03-24 17:35:02 +0700

    Add Error_handler func

---
+++ src/libgeometry/error_handler.cpp
@@ -0,0 +1,46 @@
+#include "error_handler.h"
+int error_handler(char* str) {
+    int i = 0;
+    int len = strlen("circle");
+    if (str[len] == ')') {    //check left bracket
+        for (int j = 0; j < len; j++)
+            printf(" ");
+        printf("^\nError at column %d: expected '('\n", (int)len);
+        return 0;
+    }
+    while (str[i] != '(') {
+        if (i >= len) {    //check name
+            printf("^\nError at column 0: expected 'circle'\n");
+            return 0;
+        }
+        i++;
+    }
+    i++;
+    while (str[i] != ')') {
+        if ((str[i] == '\0' || str[i] == '(' || str[i] == '\r') && (str[i - 1] == '(' || isdigit(str[i - 1]) || str[i - 1] == ' ')) {
+            for (int j = 0; j < i; j++)
+                printf(" ");
+            printf("^\nError at column %d: expected ')'\n", i);
+            return 0;
+        }
+        if ((!isdigit(str[i]) && str[i] != ' ' && str[i] != ',' && str[i] != '-' && str[i] != '.') || (str[i] == '-' && !isdigit(str[i + 1]))
+            || (str[i] == '.' && (!isdigit(str[i + 1]) || !isdigit(str[i - 1]))) || (str[i] == ',' && (!isdigit(str[i - 1]) || isdigit(str[i + 1])))) {
+            for (int j = 0; j < i; j++)
+                printf(" ");
+            printf("^\nError at column %d: expected '<double>'\n", i);
+            return 0;
+        }
+        i++;
+    }
+    i++;
+    while (str[i] != '\0') {
+        if (str[i] != ' ' && str[i] != '\r') {
+            for (int j = 0; j < i; j++)
+                printf(" ");
+            printf("^\nError at column %d: unexpected token\n", i);
+            return 0;
+        }
+        i++;
+    }
+    return 1;
+}
