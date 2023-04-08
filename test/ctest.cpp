5b781cdc8fe80e9b043859fea49d730ff8b4d422
Andrey Firsov <firstmestag@gmail.com>
2023-04-08 12:16:38 +0700

    ---

--- test/ctest.cpp
+++ test/ctest.cpp
@@ -3,12 +3,14 @@
 #include "compute.h"
 #include "intersect.h"
 
-CTEST(ctest, not_null_pointer){
+CTEST(ctest, not_null_pointer)
+{
     FILE* open = file_open();
     ASSERT_NOT_NULL(open);
 }
 
-CTEST(ctest, perimeter) {
+CTEST(ctest, perimeter)
+{
     double r[2];
     r[2] = 8.067;
     const double PI = 3.1415926535;
@@ -17,7 +19,8 @@ CTEST(ctest, perimeter) {
     ASSERT_DBL_NEAR_TOL(expected, result, 1e-1);
 }
 
-CTEST(ctest, area) {
+CTEST(ctest, area)
+{
     double r[2];
     r[2] = 8.067;
     const double PI = 3.1415926535;
@@ -26,7 +29,8 @@ CTEST(ctest, area) {
     ASSERT_DBL_NEAR_TOL(expected, result, 1e-1);
 }
 
-CTEST(ctest, intersection) {
+CTEST(ctest, intersection)
+{
     double circ1[3]={0.8, 1.9, 2.3}, circ2[3]={105, 1.9, 2.5};
     double expected = 0;
     double result = intersect(circ1, circ2);
